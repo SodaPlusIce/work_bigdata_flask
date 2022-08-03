4a9b255aa92f518489f81fb217eb4d8d33d2fb33
昝崇崇 <213201535@seu.edu.cn>
2022-08-03 17:06:56 +0800

    add ma5,ma10,ma20,ma30

--- app.py
+++ app.py
@@ -3,10 +3,48 @@ from flask import request
 import requests
 import json
 from flask_cors import CORS
+from pyspark.sql import SparkSession, Window
+from pyspark.sql import functions as F
+import tushare as ts
+import pandas as pd
 
 app = Flask(__name__)
 
-CORS(app, resources=r'/*')
+# 以下是spark的处理
+ts.set_token('96dfb6f8b1fd4d5e4972d66a49f72523746fd493cc56d921675a406a')
+def convert_timestamp(df):
+    temp=df.iloc[:, 1].tolist()
+    for i in range(len(temp)):
+        temp[i] = temp[i][0:4] + '-' + temp[i][4:6] + '-' + temp[i][6:8]
+    # print('temp', temp)
+    df.iloc[:, 1] = temp
+    return df
+def count_ma(ts_code,startdate,enddate):
+    spark=SparkSession.builder.appName("count_ma5").master("local[*]").getOrCreate()
+    sc=spark.sparkContext
+    pro = ts.pro_api()
+    df=pro.query('daily', ts_code=ts_code, start_date=startdate, end_date=enddate)
+
+    df=pd.DataFrame(df)
+    df = convert_timestamp(df)
+    df=spark.createDataFrame(df)
+
+    df=df.withColumn('trade_date',df.trade_date.cast('timestamp'))
+    day=lambda x:x*86400
+    dayitem=['5','10','20','30']
+    for ma_day in dayitem:
+     winSpec = Window.partitionBy('ts_code').orderBy(F.col('trade_date').cast('long')).rangeBetween(-day(int(ma_day)), 0)
+     df = df.withColumn('ma'+ma_day, F.avg('close').over(winSpec))
+    return df
+def calMa5BySpark(ts_code,datebefore,date):
+    ma_result = count_ma(ts_code, datebefore, date)  # args explanation:股票代码、开始时间、结束时间
+    # 请注意传来的开始结束时间,是‘YYYYMMDD’的字符串形式
+    # ma_result.show()
+    ma_result = ma_result.toPandas()  # 此行是将spark的df转换为pandas的df，如果你很熟悉spark的df，那么可以不用加,不过还是建议用pandas的dataframe
+    # print(ma_result)
+    return ma_result
+
+CORS(app, resources=r'/*') # 解决跨域
 
 @app.route('/getDataByCode')
 def getDataByCode():  # put application's code here
@@ -24,12 +62,19 @@ def getDataByCode():  # put application's code here
     res_json = res.json()
     data = res_json["data"]["items"]
     i=0
+    # 获取ma5等值
+    # data=data[:34]# 取近34天（0-33）的数据（第30天的要用到30-34的数据来计算ma5）
+    data=data[:30]# 取近30天（0-29）的数据
+    madata=calMa5BySpark(ts_code,data[29][1],data[0][1])
     while(i<len(data)):
         vol=data[i][9]
         data[i]=data[i][:6]
         data[i].append(vol)
+        data[i].append(round(madata["ma5"][i],2))
+        data[i].append(round(madata["ma10"][i],2))
+        data[i].append(round(madata["ma20"][i],2))
+        data[i].append(round(madata["ma30"][i],2))
         i+=1
-    data=data[:34]# 取近34天（0-33）的数据（第30天的要用到30-34的数据来计算ma5）
     return json.dumps(data)
 
 
--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+import tushare as ts
+import pandas as pd
+from pyspark import SparkConf, SparkContext
+import warnings
+ts.set_token('96dfb6f8b1fd4d5e4972d66a49f72523746fd493cc56d921675a406a')
+from pyspark.sql import SparkSession, Window
+from pyspark.sql import functions as F
+# 此函数是将传入的startdate和enddate转换为时间戳的字符串形式,即‘YYYY-MM-DD’
+def convert_timestamp(df):
+    temp=df.iloc[:, 1].tolist()
+    for i in range(len(temp)):
+        temp[i] = temp[i][0:4] + '-' + temp[i][4:6] + '-' + temp[i][6:8]
+    # print('temp', temp)
+    df.iloc[:, 1] = temp
+    return df
+def count_ma(ts_code,startdate,enddate):
+    spark=SparkSession.builder.appName("count_ma5").master("local[*]").getOrCreate()
+    sc=spark.sparkContext
+    pro = ts.pro_api()
+    df=pro.query('daily', ts_code=ts_code, start_date=startdate, end_date=enddate)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    df=pd.DataFrame(df)
+    df = convert_timestamp(df)
+    df=spark.createDataFrame(df)
 
+    df=df.withColumn('trade_date',df.trade_date.cast('timestamp'))
+    day=lambda x:x*86400
+    dayitem=['5','10','20','30']
+    for ma_day in dayitem:
+     winSpec = Window.partitionBy('ts_code').orderBy(F.col('trade_date').cast('long')).rangeBetween(-day(int(ma_day)), 0)
+     df = df.withColumn('ma'+ma_day, F.avg('close').over(winSpec))
+    return df
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+   ma_result=count_ma('000001.SZ','20220230','20220330')# args explanation:股票代码、开始时间、结束时间
+                                                     # 请注意传来的开始结束时间,是‘YYYYMMDD’的字符串形式
+   ma_result.show()
+   ma_result=ma_result.toPandas() # 此行是将spark的df转换为pandas的df，如果你很熟悉spark的df，那么可以不用加,不过还是建议用pandas的dataframe
+   print(ma_result["trade_date"][0])
+   print(ma_result["ma5"][0])
